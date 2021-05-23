a22c8e8262822c723d2d1d9de68c8b106ba6a3a8
DmitriyBessalov <P88jaFuZRKNr>
2021-05-23 23:03:04 +0300

    исправление git

--- apps/base/views.py
+++ apps/base/views.py
@@ -10,5 +10,5 @@ def index(request):
 
 @csrf_exempt
 def git_update(request):
-    result = subprocess.run('git reset --hard origin/master && git pull https://github.com/DmitriyBessalov/social_network.git', shell=True, stdout=subprocess.PIPE)
+    result = subprocess.run('git reset --hard origin/master && git pull https://github.com/DmitriyBessalov/buddha_chess.git', shell=True, stdout=subprocess.PIPE)
     return HttpResponse(result.stdout)
