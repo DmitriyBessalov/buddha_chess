84ca469592884e8e9ee0c976fc0e8bde6c9cec5a
DmitriyBessalov <P88jaFuZRKNr>
2021-06-04 23:43:40 +0300

    Частичное обновление страницы "Начать игру"

--- nodejs/src/App.jsx
+++ nodejs/src/App.jsx
@@ -4,7 +4,7 @@ import {Container} from "@material-ui/core";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Auth} from "./components/Auth/Auth";
 import {Article} from "./components/Article/Article";
-import {Game} from "./components/Game/Game";
+import {StartGame} from "./components/StartGame/StartGame";
 import {StylesProvider} from "@material-ui/core/styles";
 
 export const Logined = React.createContext()
@@ -27,7 +27,7 @@ export const App = () => {
             <Container>
               <Switch>
                   <Route path="/ru/auth" component={Auth}/>
-                  <Route path="/ru/играть" component={Game}/>
+                  <Route path="/ru/играть" component={StartGame}/>
                   <Route path="/" component={Article}/>
               </Switch>
             </Container>
